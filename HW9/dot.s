72fd3e64d20d508154d7ec38946a4b0cdeadb9bc
Kevin Ward <kward3@stevens.edu>
2020-04-21 13:54:01 -0400

    Finished HW9

--- HW9/dot.s
+++ HW9/dot.s
@@ -13,3 +13,20 @@ _Z3dotPdS_:
 	@if the vector is BIG
 	@ use [r0]
 	@ add r0, #8
+
+	vldr		d1, [r0]
+	vldr		d2, [r1]
+	vmul.f64	d3, d1, d2
+	vadd.f64	d0, d3
+	vldr		d1, [r0, #8]
+	vldr		d2, [r1, #8]
+	vmul.f64	d3, d1, d2
+	vadd.f64	d0, d3
+	vldr		d1, [r0, #16]
+	vldr		d2, [r1, #16]
+	vmul.f64	d3, d1, d2
+	vadd.f64	d0, d3
+	bx		lr
+	
+	
+	
